8e363833907715793dd766c11e64b75b2d4a4cdb
Jared Nishikawa <jared.nishikawa@gmail.com>
2020-04-26 11:44:26 -0600

    Scanner (#3)
    * initial draft of scanner and parser
    
    * some comments for clarity
    
    * scanner (tokenizer) is working, and basic parser implemented
    
    * implemented tree structure, reorganized package
    
    * golint-ed
    
    * converted parser and scanner back to top-level
    
    * fixed properties with multi values a la AW[kk][ji]
    
    * isWhiteSpace -> isWhitespace

---
+++ game/game.go
@@ -0,0 +1,28 @@
+package game
+
+// Game contains the game tree information
+type Game struct {
+	Root *Node
+}
+
+// NewGame creates a Game struct
+func New() *Game {
+	g := &Game{
+		Root: NewNode(),
+	}
+	return g
+}
+
+// Node contains Properties, Children nodes, and Parent node
+type Node struct {
+	Properties map[string][]string
+	Children   []*Node
+	Parent     *Node
+}
+
+// NewNode creates a Node
+func NewNode() *Node {
+	return &Node{
+		Properties: make(map[string][]string),
+	}
+}
