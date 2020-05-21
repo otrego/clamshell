989925de9b038d64df7eed38c9bc09cc61bc44f2
Josh Hoak <jrhoak@gmail.com>
2020-05-21 13:02:29 -0700

    Finish basic gRPC / gRPC Gateway service
    This allows you to do:
    
    $ go run main.go
    $ curl localhost:10000/v1/echo/foo
    {"content":"this is a foo message"}
    
    Also, while I'm here, generate and serve swagger.json

--- regenerate-proto.sh
+++ regenerate-proto.sh
@@ -17,3 +17,20 @@ protoc --grpc-gateway_out=logtostderr=true,paths=source_relative:. \
   -I ./third_party/googleapis \
   -I ./third_party/grpc-gateway \
   ./server/api/*.proto
+
+protoc --swagger_out=logtostderr=true:. \
+  -I . \
+  -I ./third_party/googleapis \
+  -I ./third_party/grpc-gateway \
+  ./server/api/*.proto
+
+# Manually go-embed the swagger into a go file.
+#
+# This is quite hacky -- we should use go-bindata or even go generate. For now,
+# it's mega simple. See https://github.com/otrego/clamshell/issues/40
+SWAG_GO="./server/api/api.swagger.go"
+echo 'package api' > "${SWAG_GO}"
+echo '' >> "${SWAG_GO}"
+echo 'const Swagger = `' >> "${SWAG_GO}"
+cat ./server/api/api.swagger.json | sed 's/`/'"'"'/g' >> "${SWAG_GO}"
+echo '`' >> "${SWAG_GO}"
--- server/grpc/grpc.go
+++ server/grpc/grpc.go
@@ -2,12 +2,16 @@
 package grpc
 
 import (
+	"context"
 	"fmt"
-	"net"
+	"io"
+	"net/http"
+	"strings"
 
 	log "github.com/golang/glog"
 	"google.golang.org/grpc"
 
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	pb "github.com/otrego/clamshell/server/api"
 	"github.com/otrego/clamshell/server/echo"
 )
@@ -21,20 +25,66 @@ type Options struct {
 // Run starts a gRPC server.
 //
 // The server will be shutdown when "ctx" is canceled.
+//
+// This method of serving both gRPC and gRPC-Gateway is inspired by:
+// https://github.com/philips/grpc-gateway-example/blob/master/cmd/serve.go
 func Run(opts *Options) {
+	ctx := context.Background()
+
+	// Listen on addr
 	addr := fmt.Sprintf("localhost:%d", opts.Port)
-	lis, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalf("failed to dial to addr %s: %v", addr, err)
+
+	// Initialize both the http and grpc handlers.
+	grpcServer := initGRPCServer()
+	mux := http.NewServeMux()
+
+	serveSwaggerDefinition(mux)
+	serveGRPCGateway(ctx, mux)
+
+	log.Infof("listening on %s", addr)
+	if err := serve(ctx, addr, mux, grpcServer); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
-	grpcServer := initGRPCServer(opts)
-	log.Infof("listening on :%d", opts.Port)
-	grpcServer.Serve(lis)
 }
 
-func initGRPCServer(_ *Options) *grpc.Server {
+func serve(ctx context.Context, addr string, mux *http.ServeMux, grpcServer *grpc.Server) error {
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: handleGRPCAndHTTP(grpcServer, mux),
+	}
+	return srv.ListenAndServe()
+}
+
+func initGRPCServer() *grpc.Server {
+	// TODO(kashomon): It's possible we don't need a gRPC server at all and can
+	// just use gRPC Gateway directly and skip serving a gRPC endpoint.
 	var gopts []grpc.ServerOption
 	grpcServer := grpc.NewServer(gopts...)
 	pb.RegisterEchoServiceServer(grpcServer, &echo.EchoServer{})
 	return grpcServer
+}
+
+// serveSwaggerDefinition serves swagger definitions from /swagger.json
+func serveSwaggerDefinition(mux *http.ServeMux) {
+	mux.HandleFunc("/swagger.json", func(w http.ResponseWriter, req *http.Request) {
+		io.Copy(w, strings.NewReader(pb.Swagger))
+	})
+}
+
+func serveGRPCGateway(ctx context.Context, mux *http.ServeMux) {
+	// TODO(kashomon): Add TLS / credentials.
+	gwmux := runtime.NewServeMux()
+	pb.RegisterEchoServiceHandlerServer(ctx, gwmux, &echo.EchoServer{})
+	mux.Handle("/", gwmux)
+}
+
+// handleGRPCAndHTTP muxes between gRPC and HTTP based on the header type.
+func handleGRPCAndHTTP(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	})
 }
