4a7d3e6e7ddd8e46e53553ee07d7b9b35fb3903f
Michael <mdhoak@gmail.com>
2020-06-14 14:43:22 -0700

    Update terraform to deploy (#53)
    Adds Initial terraform config that correctly boots up a server that serves a hello-world on :8080
    
    Additionally, this adds a deploy-on-merge action.

---
+++ tf/variables.tf
@@ -0,0 +1,41 @@
+variable "project_id" {
+  description = "The project ID to deploy resources into"
+  default = "otrego-dev"
+}
+
+variable "subnetwork_project" {
+  description = "The project ID where the desired subnetwork is provisioned"
+  default     = "otrego-dev"
+}
+
+variable "subnetwork" {
+  description = "The name of the subnetwork to deploy instances into"
+  default     = "default"
+}
+
+variable "instance_name" {
+  description = "The desired name to assign to the deployed instance"
+  default     = "hello-world-container-vm"
+}
+
+variable "zone" {
+  description = "The GCP zone to deploy instances into"
+  type        = string
+  default     = "us-west1-a"
+}
+
+variable "client_email" {
+  description = "Service account email address"
+  type        = string
+  default     = ""
+}
+
+variable "cos_image_name" {
+  description = "The forced COS image to use instead of latest"
+  default     = "cos-stable-77-12371-89-0"
+}
+
+variable "api_docker_image" {
+  description = "Docker Image of the Otrego API"
+  type        = string
+}
