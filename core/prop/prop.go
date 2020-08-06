5a2d595698323e8f57ae371cefb052748c150ec6
Jeremy N <jeremy.a.nelson@gmail.com>
2020-08-06 02:00:28 +0100

    Separated Prop into separate package; added test.

---
+++ core/prop/prop.go
@@ -0,0 +1,18 @@
+package prop
+
+import (
+)
+
+
+type Field string
+
+var ValidProperties = map[Field]bool{"AB": true, "AE": true, "AN": true, "AP": true, "AR": true, "AW": true, "B": true, "BL": true, "BM": true, "BR": true, "BT": true, "C": true, "CA": true, "CP": true, "CR": true, "DD": true, "DM": true, "DO": true, "DT": true, "EV": true, "FF": true, "FG": true, "GB": true, "GC": true, "GM": true, "GN": true, "GW": true, "HA": true, "HO": true, "IT": true, "KM": true, "KO": true, "LB": true, "LN": true, "MA": true, "MN": true, "N": true, "OB": true, "ON": true, "OT": true, "OW": true, "PB": true, "PC": true, "PL": true, "PM": true, "PW": true, "RE": true, "RO": true, "RU": true, "SL": true, "SO": true, "SQ": true, "ST": true, "SZ": true, "TB": true, "TE": true, "TM": true, "TR": true, "TW": true, "UC": true, "US": true, "V": true, "VW": true, "W": true, "WL": true, "WR": true, "WT": true,}
+
+// Validate returns true if Prop is an accepted SGF property.
+func Validate(field Field) (bool) {
+	if _, ok := ValidProperties[field]; ok {
+		return true
+	}
+	return false
+}
+
