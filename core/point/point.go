8e8ae9dee54b7aa9adff41ed24349b3f0f811d80
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-09-01 03:26:15 -0700

    Package Point Translation Methods
    Point translation methods added to Package point.
    
    The translation methods translate points from from Int Points to SGF Points, and vice versa.

--- core/point/point.go
+++ core/point/point.go
@@ -1,5 +1,9 @@
-// Package point is a basic package for points.
+// // Package point is a basic package for points.
 package point
+
+import (
+	"strings"
+)
 
 // Point is a basic point. Although simple, the member variables are kept
 // private to ensure that Point remains immutable.
@@ -21,3 +25,34 @@ func (p *Point) X() int64 { return p.x }
 
 // Y returns the y-value.
 func (p *Point) Y() int64 { return p.y }
+
+// Kilo Foxtrot addition below
+// Three point translation methods, one is an alternate
+
+// 1. '*Point' immutable int point, to SGF(String of two letters) Point
+func translateToSGFPt01(pt *Point) string {
+	sgfPtX := string(rune((pt.X()) + 97))
+	sgfPtY := string(rune((pt.Y()) + 97))
+	// two lower case letters
+	// return sgfPtX + sgfPtY
+	return strings.ToLower(sgfPtX) + strings.ToLower(sgfPtY)
+}
+
+// Alternate, in case the incoming point is not immutable
+// 2. 'Point' mutable int point, to SGF(String of two letters) Point
+func translateToSGFPt02(pt Point) string {
+	sgfPtX := string(rune((pt.X()) + 97))
+	sgfPtY := string(rune((pt.Y()) + 97))
+	// two lower case letters
+	// return sgfPtX + sgfPtY
+	return strings.ToLower(sgfPtX) + strings.ToLower(sgfPtY)
+}
+
+// 3. 'p' SGF(two letter string) Point, to '*Point' immutable Int Point
+func translateToINTPt01(sgfPt string) *Point {
+	sgfPtX := sgfPt[0]
+	sgfPtY := sgfPt[1]
+	x := int(sgfPtX) - 97
+	y := int(sgfPtY) - 97
+	return New(int64(x), int64(y))
+}
