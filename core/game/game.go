286ed097f2c332d4314c7f0a134329ff3c129bfc
Josh Hoak <jrhoak@gmail.com>
2020-09-07 20:50:17 -0700

    Update color / game.

--- core/game/game.go
+++ core/game/game.go
@@ -1,11 +1,23 @@
 package game
 
-// Game contains the game tree information
+import (
+	"github.com/otrego/clamshell/core/color"
+	"github.com/otrego/clamshell/core/point"
+)
+
+// A Move is location + a color. A pass is represented by a Move with defined
+// color but no point specified.
+type Move struct {
+	Color color.Color
+	Point *point.Point
+}
+
+// Game contains the game tree information for a go game.
 type Game struct {
 	Root *Node
 }
 
-// New creates a Game struct.
+// New creates a Game.
 func New() *Game {
 	g := &Game{
 		Root: NewNode(),
@@ -13,14 +25,26 @@ func New() *Game {
 	return g
 }
 
-// Node contains Properties, Children nodes, and Parent node
+// Node contains Properties, Children nodes, and Parent node.
 type Node struct {
+	// Placements are stones that are used for setup, but actual moves. For
+	// example, handicap stones will be in in placements.
+	Placements []*Move
+
+	// Move indicates a move in the game.
+	Move *Move
+
+	// Properties contain all the raw/unprocessed properties
 	Properties map[string][]string
-	Children   []*Node
-	Parent     *Node
+
+	// Children of this position.
+	Children []*Node
+
+	//
+	Parent *Node
 }
 
-// NewNode creates a Node
+// NewNode creates a Node.
 func NewNode() *Node {
 	return &Node{
 		Properties: make(map[string][]string),
