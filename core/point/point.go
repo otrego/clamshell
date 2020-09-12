97ad526d0871ae9383c56db650096cea960bdac8
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-09-12 04:27:29 -0700

    Updated point.go and point_test.go

--- core/point/point.go
+++ core/point/point.go
@@ -1,12 +1,24 @@
 // Package point is a basic package for points.
 package point
 
+import (
+	"bytes"
+)
+
 // Point is a basic point. Although simple, the member variables are kept
 // private to ensure that Point remains immutable.
 type Point struct {
 	x int64
 	y int64
 }
+
+// PointSGFSlce is a slice/Array translation reference between integer
+// -Point (index position) and string SGF-Point (byte/char) values
+var PointSGFSlce = []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+	'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+	'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+	'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+	'V', 'W', 'X', 'Y', 'Z', 'x', 'y', 'z'}
 
 // New creates a new immutable Point.
 func New(x, y int64) *Point {
@@ -21,3 +33,34 @@ func (p *Point) X() int64 { return p.x }
 
 // Y returns the y-value.
 func (p *Point) Y() int64 { return p.y }
+
+// ToSGF() converts a pointer-type (immutable) *Point
+// to an SGF Point (two letter string). The returned value is 0-indexed.
+func (pt *Point) ToSGF() string {
+	if (pt != nil) && (pt.X() <= 51) && (pt.Y() <= 51) {
+		sgfX := string(PointSGFSlce[pt.X()])
+		sgfY := string(PointSGFSlce[pt.Y()])
+		return sgfX + sgfY
+	} else {
+		panic("Error: *Point entries must not be nil for either" +
+			" x y entries and have 0 <= x, y <= 51 values. ")
+	}
+}
+
+// NewFromSGF converts an SGF point (
+// two letter string, 0-indexed) to a pointer-type (immutable) *Point.
+func NewFromSGF(sgfPt string) *Point {
+	if (sgfPt != "") && (len(sgfPt) == 2) {
+		xIndx := bytes.Index(PointSGFSlce, []byte(string(sgfPt[0])))
+		yIndx := bytes.Index(PointSGFSlce, []byte(string(sgfPt[1])))
+
+		// x := int64(sgfPt[0]) - aValue
+		// y := int64(sgfPt[1]) - aValue
+		return New(int64(xIndx), int64(yIndx))
+	} else {
+		panic("Error: SGF string entries must not be empty and must" +
+			" be of length = 2 byte/byte.  ")
+		// return New(99, 99)
+	}
+
+}
--- core/point/point_test.go
+++ core/point/point_test.go
@@ -13,3 +13,117 @@ func TestCreate(t *testing.T) {
 		t.Errorf("p.Y()=%v, expected %v", gotY, 2)
 	}
 }
+
+func TestPointSGFTranslation(t *testing.T) {
+	type Point struct {
+		x int64
+		y int64
+	}
+
+	// First test translation from integer-point to SGF-string-point
+	testToSGFCases := []struct {
+		desc string
+		in   Point
+		want string
+	}{
+		{
+			desc: "Point => SGF",
+			in: Point{
+				x: 8,
+				y: 16,
+			},
+			want: "iq",
+		},
+		{
+			desc: "Point => SGF",
+			in: Point{
+				x: 12,
+				y: 5,
+			},
+			want: "mf",
+		},
+		{
+			desc: "Point => SGF",
+			in: Point{
+				x: 33,
+				y: 8,
+			},
+			want: "Hi",
+		},
+		{
+			desc: "Point => SGF",
+			in: Point{
+				x: 40,
+				y: 51,
+			},
+			want: "OZ",
+		},
+	}
+
+	for _, tc := range testToSGFCases {
+		t.Run(tc.desc, func(t01 *testing.T) {
+			toSGFOut := New(tc.in.x, tc.in.y).ToSGF()
+			if t01 == nil || toSGFOut != tc.want {
+				t01.Errorf("%q.ToSGF() = %q, but wanted %q", tc.in,
+					toSGFOut, tc.want)
+			}
+		})
+	}
+
+	// Second test translation from SGF-string-point to integer-point
+	testToPointCases := []struct {
+		desc string
+		in   string
+		want *Point
+	}{
+		{
+			desc: "SGF => Point",
+			in:   "iq",
+			want: &Point{
+				x: 8,
+				y: 16,
+			},
+		},
+		{
+			desc: "SGF => Point",
+			in:   "mf",
+			want: &Point{
+				x: 12,
+				y: 5,
+			},
+		},
+		{
+			desc: "SGF => Point",
+			in:   "Hi",
+			want: &Point{
+				x: 33,
+				y: 8,
+			},
+		},
+		{
+			desc: "SGF => Point",
+			in:   "OZ",
+			want: &Point{
+				x: 40,
+				y: 51,
+			},
+		},
+	}
+
+	for _, tc := range testToPointCases {
+		t.Run(tc.desc, func(t01 *testing.T) {
+			toPointOut := NewFromSGF(tc.in)
+			// include the point.go *Point type X() Y() getters below
+			pointX := toPointOut.X()
+			pointY := toPointOut.Y()
+			if t01 == nil || pointX != tc.want.x {
+				t01.Errorf("%q.toPointOut.x = %q, but wanted %q", tc.in,
+					pointX, tc.want.x)
+			}
+			if t01 == nil || pointY != tc.want.y {
+				t01.Errorf("%q.toPointOut.y = %q, but wanted %q", tc.in,
+					pointY, tc.want.y)
+			}
+		})
+	}
+}
