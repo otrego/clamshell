4d1c569cddf5adff3cba5a07a5a9673730c5e7d9
Josh Hoak <jrhoak@gmail.com>
2020-09-12 19:21:09 -0700

    Add a treepath implementation (#82)
    Add a treepath implementation, to parse and use paths.

---
+++ core/treepath/treepath.go
@@ -0,0 +1,167 @@
+// Package treepath provides functionality for manipulating treepaths.
+//
+// Treepaths are string-encodings of paths through trees.
+//
+// Treepaths come originially from
+// https://github.com/Kashomon/glift-core/blob/master/src/rules/treepath.js
+package treepath
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"github.com/otrego/clamshell/core/game"
+)
+
+// A Treepath is a list of variations that says how to travel through a tree of
+// moves. And has two forms a list version and a string version. First, the
+// list-version:
+//
+//    [0,1,0]
+//
+// Means we will first take the 0th variation, then we will take the 1ist
+// variation, and lastly we will take the 0th variation again. For
+// convenience, the treepath can also be specified by a string, which is where
+// the fun begins. At its simpliest,
+//
+//    [0,0,0] becomes 0.0.0
+type Treepath []int
+
+type parseState int
+
+const (
+	// variationState means we are looking for numbers for the variation number. Can
+	// either transition to either separator (flush) or repeat (flush and then
+	// repeat N times
+	variationState parseState = iota
+	// repeat means we repeat the previous variation 'n' times.
+	repeatState
+)
+
+// Parse parses a treepath from a string to an array of ints (Treepath).
+//
+// There are a couple different string short-hands that make using treepaths
+// a little easier
+//
+//    0.1:2   Take the 0th variation, then repeat taking the 1st varation twice
+//
+// Treepaths say how to get from position n to position m.  Thus the numbers are
+// always variations, separated by '.' except in the case of A:B syntax, which
+// means repeat A for B times.
+//
+//
+// Some examples:
+//
+//    0             becomes [0]
+//    1             becomes [1]
+//    53            becomes [53] (the 53rd variation)
+//    2.3           becomes [2,3]
+//    0.0.0.0       becomes [0,0,0]
+//    0:4           becomes [0,0,0,0]
+//    1:4           becomes [1,1,1,1]
+//    1.2:1.0.2:3   becomes [1,2,0,2,2,2]
+func Parse(path string) (Treepath, error) {
+	out := Treepath{}
+	curState := variationState
+	buf := &strings.Builder{}
+	prevChar := 0
+
+	convertBuffer := func(idx int) (int, error) {
+		if buf.Len() == 0 {
+			return 0, fmt.Errorf("error parsing fragment %q at index %d: separators '.', ':', EOL must be proceeded by digit", path, idx)
+		}
+		n, err := strconv.Atoi(buf.String())
+		if err != nil {
+			return 0, fmt.Errorf("error parsing number in fragment %q at index %d: %v", path, idx, err)
+		}
+		buf = &strings.Builder{}
+		return n, nil
+	}
+
+	// Use a sentinal newline character so that it's clear when to flush the final
+	// charactrs.
+	path += "\n"
+
+	// The rough approach is to have a simple parser modeled as a 2-state DFA.
+	//
+	// VARIATION: means we're parsing a normal variation number. We flush the
+	// buffer data when we hit a '.' rune.
+	//
+	// REPEAT: means we have seen a ':' instead of a '.', so we're going to repeat
+	// the previous number once we parse the next number.
+	//
+	//  --'.'--- <------------
+	//  |      |             |
+	//  V      ^             ^
+	// VARIATION --':'-->  REPEAT
+	//   |                   V
+	//   EOL <----------------
+	//   |
+	//   V
+	//   End
+	for idx, c := range path {
+		if unicode.IsDigit(c) {
+			buf.WriteRune(c)
+		} else if c == '.' || c == '\n' {
+			n, err := convertBuffer(idx)
+			if err != nil {
+				return nil, err
+			}
+
+			// State transitions
+			if curState == variationState {
+				// Variation => Variation (& flush)
+				out = append(out, n)
+			} else if curState == repeatState {
+				// Repeat => Variation (& flush)
+				for i := 0; i < n; i++ {
+					out = append(out, prevChar)
+				}
+				curState = variationState
+				prevChar = 0
+			} else {
+				return nil, fmt.Errorf("error parsing fragment %q at index %d: unknown state %v", path, idx, curState)
+			}
+		} else if c == ':' {
+			n, err := convertBuffer(idx)
+			if err != nil {
+				return nil, err
+			}
+
+			// State transitions
+			if curState == variationState {
+				// Variation => Repeat
+				prevChar = n
+				curState = repeatState
+			} else if curState == repeatState {
+				// Repeate => Repeat: Invalid
+				return nil, fmt.Errorf("error parsing fragment %q at index %d: repeat char ':' cannot be followed by another repeat ':'", path, idx)
+			} else {
+				return nil, fmt.Errorf("error parsing fragment %q at index %d: unknown state %v", path, idx, curState)
+			}
+		} else {
+			return nil, fmt.Errorf("error parsing fragment %q at index %d: unexpected character %q", path, idx, c)
+		}
+	}
+	return out, nil
+}
+
+// Apply applies a treepath to a node, moving down a tree of moves. This
+// takes the variation listed in the treepath until either:
+//
+// 1. There are no more variation numbers in the treepath.
+// 2. There is not a child with the given variation number.
+func (tp Treepath) Apply(n *game.Node) *game.Node {
+	curNode := n
+	for _, v := range tp {
+		if v < len(curNode.Children) {
+			// Assume there are no gaps. If there are, parsing failed us.
+			curNode = curNode.Children[v]
+		} else {
+			break
+		}
+	}
+	return curNode
+}
---
+++ core/treepath/treepath_test.go
@@ -0,0 +1,153 @@
+package treepath
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/otrego/clamshell/core/errcheck"
+	"github.com/otrego/clamshell/core/parser"
+)
+
+func TestParse(t *testing.T) {
+	testCases := []struct {
+		desc         string
+		path         string
+		exp          []int
+		expErrSubstr string
+	}{
+		{
+			desc: "0th varation",
+			path: "0",
+			exp:  []int{0},
+		},
+		{
+			desc: "first move",
+			path: "1",
+			exp:  []int{1},
+		},
+		{
+			desc: "variation 10",
+			path: "10",
+			exp:  []int{10},
+		},
+		{
+			desc: "move 2.3",
+			path: "2.3",
+			exp:  []int{2, 3},
+		},
+		{
+			desc: "varitaion 3",
+			path: "3",
+			exp:  []int{3},
+		},
+		{
+			desc: "move 2.0",
+			path: "2.0",
+			exp:  []int{2, 0},
+		},
+		{
+			desc: "move 0.0.0.0 = 4 moves",
+			path: "0.0.0.0",
+			exp:  []int{0, 0, 0, 0},
+		},
+		{
+			desc: "move 0.0:3 = 4 moves",
+			path: "0.0:3",
+			exp:  []int{0, 0, 0, 0},
+		},
+		{
+			desc: "move 0:4 = 4 moves",
+			path: "0:4",
+			exp:  []int{0, 0, 0, 0},
+		},
+		{
+			desc: "move 0:4 = 4 moves @ var 1",
+			path: "1:4",
+			exp:  []int{1, 1, 1, 1},
+		},
+		{
+			desc: "complicated pattern",
+			path: "1.2:1.0.3:3",
+			exp:  []int{1, 2, 0, 3, 3, 3},
+		},
+		// Error cases
+		{
+			desc:         "move a:2 is invalid - not a number",
+			path:         "a:2",
+			expErrSubstr: "unexpected char",
+		},
+		{
+			desc:         "move -1:2 is invalid - negatives are not allowed",
+			path:         "-1:2",
+			expErrSubstr: "unexpected char",
+		},
+		{
+			desc:         "move 1::2 is invalid - no repeat separators",
+			path:         "1::2",
+			expErrSubstr: "separator",
+		},
+		{
+			desc:         "move 1..2 is invalid - no repeat separators",
+			path:         "1..2",
+			expErrSubstr: "separator",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := Parse(tc.path)
+
+			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
+			if cerr != nil {
+				t.Error(cerr)
+				return
+			}
+			if err != nil {
+				return
+			}
+
+			if !cmp.Equal(got, Treepath(tc.exp)) {
+				t.Errorf("Parse(%v)=%v, but expected %v", tc.path, got, tc.exp)
+			}
+		})
+	}
+}
+
+func TestApplyPath(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		initPath string
+		game     string
+		expProps map[string][]string
+	}{
+		{
+			desc:     "first move",
+			initPath: "0.0",
+			game:     "(;GM[1];B[pd]C[foo])",
+			expProps: map[string][]string{
+				"C": []string{"zed"},
+				"B": []string{"pd"},
+			},
+		},
+	}
+	t.Skip("Parsing is currently incorrect:Re-enable test once https://github.com/otrego/clamshell/issues/83 is fixed")
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			g, err := parser.FromString(tc.game).Parse()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			path, err := Parse(tc.initPath)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			n := path.Apply(g.Root)
+
+			if !cmp.Equal(n.Properties, tc.expProps) {
+				t.Errorf("path.Apply(root)=%v, expected %v. Diff=%v", n.Properties, tc.expProps, cmp.Diff(n.Properties, tc.expProps))
+			}
+		})
+	}
+}
