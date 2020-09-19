6d55677ca7a95dd7d385acdebcedbf2e86a4aed1
Jeremy N <jeremy.a.nelson@gmail.com>
2020-09-19 13:53:24 +0100

    Removed redundant test code.

--- core/prop/prop_test.go
+++ core/prop/prop_test.go
@@ -8,16 +8,10 @@ import (
 func TestValidate(t *testing.T) {
 
 	var valid prop.Field = "TB"
-	var invalid prop.Field = "TQB"
 
 	test := prop.Validate(valid)
 	if test != true {
 		t.Errorf("Valid SGF field returned %v; want true", test)
-	}
-
-	test = prop.Validate(invalid)
-	if test != false {
-		t.Errorf("Invalid SGF field returned %v; want false", test)
 	}
 }
 
