3bf940d954a1bc488701600d073f237c70b280d8
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-10-03 05:44:48 -0700

    Refined point.go and point_test.go with translation fmt.Errof parameters.

--- core/point/point.go
+++ core/point/point.go
@@ -12,9 +12,9 @@ type Point struct {
 	y int64
 }
 
-// pointToSgfMap is a translation reference between int64 Point
+// pointToSgfRef is a translation reference between int64 Point
 // and string SGF-Point (rune) values
-var pointToSgfMap = map[int64]rune{
+var pointToSgfRef = map[int64]rune{
 	0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g',
 	7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n',
 	14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u',
@@ -26,14 +26,14 @@ var pointToSgfMap = map[int64]rune{
 }
 
 // sgfToPointRef is an anonymous function that runs and inverts the
-// pointToSgfMap to create the reverse translation reference
-var sgfToPointRef = func(mapIn map[int64]rune) map[rune]int64 {
-	mapElem := make(map[rune]int64)
-	for key, val := range mapIn {
-		mapElem[val] = key
+// pointToSgfRef to create the reverse translation reference
+var sgfToPointRef = func(in map[int64]rune) map[rune]int64 {
+	elem := make(map[rune]int64)
+	for key, val := range in {
+		elem[val] = key
 	}
-	return mapElem
-}(pointToSgfMap)
+	return elem
+}(pointToSgfRef)
 
 // New creates a new immutable Point.
 func New(x, y int64) *Point {
@@ -51,25 +51,23 @@ func (pt *Point) Y() int64 { return pt.y }
 
 // ToSGF converts a pointer-type (immutable) *Point to an SGF Point (
 // two letter string).
-// The returned value is 0-indexed.
 func (pt *Point) ToSGF() (string, error) {
-	var sgfOut string
-	var err01 = fmt.Errorf("")
-	if (0 <= pt.X()) && (pt.X() <= 51) && (0 <= pt.Y()) && (pt.Y() <= 51) {
-		sgfX := string(pointToSgfMap[pt.X()])
-		sgfY := string(pointToSgfMap[pt.Y()])
-		sgfOut = sgfX + sgfY
-		err01 = nil
+	if (pt.X() < 0) && (51 < pt.X()) && (pt.Y() < 0) && 51 < (pt.Y()) {
+		return "", fmt.Errorf(
+			"*Point int64 x and y value entries must" +
+				" be greater than or equal to 0, " +
+				"and less than or equal to 51. ")
 	} else {
-		sgfOut = ""
-		err01 = fmt.Errorf("*Point int64 x and y value entries must" +
-			" be greater than or equal to 0, " +
-			"and less than or equal to 51. ")
+		return string(pointToSgfRef[pt.X()]) + string(
+			pointToSgfRef[pt.Y()]), nil
 	}
-	return sgfOut, err01
 }
 
-// String() method to represent and print a Point, useful for debugging and test purposes
+// The below function and related elements were contributed by
+// un nuovo amico GitHub dall'Italia, di nome ilmanzo
+
+// String() method to represent and print a Point,
+// useful for debugging and test purposes - ilmanzo contributing
 func (pt Point) String() string {
 	return fmt.Sprintf("{%d,%d}", pt.x, pt.y)
 }
@@ -78,19 +76,12 @@ func (pt Point) String() string {
 // two letter string) to a pointer-type (immutable) *Point (
 // (struct with two rune/char values).
 func NewFromSGF(sgfPt string) (*Point, error) {
-	var intX int64
-	var intY int64
-	var err02 = fmt.Errorf("")
-	if (sgfPt != "") && (len(sgfPt) == 2) {
-		intX = sgfToPointRef[rune(sgfPt[0])]
-		intY = sgfToPointRef[rune(sgfPt[1])]
-		err02 = nil
+	if (sgfPt == "") || (len(sgfPt) != 2) {
+		return nil, fmt.Errorf(
+			"SGF string x and y value entries mustnon-empty and of" +
+				" length 2 (runes/chars). ")
 	} else {
-		intX = 99
-		intY = 99
-		err02 = fmt.Errorf("SGF string x and y value entries must non" +
-			"-empty and of length 2 (runes/chars). ")
+		return New(sgfToPointRef[rune(sgfPt[0])],
+			sgfToPointRef[rune(sgfPt[1])]), nil
 	}
-	return New(intX, intY), err02
-
 }
