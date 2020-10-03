33caa319349dbda469f9f17118635bab339a8f46
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-10-03 05:50:28 -0700

    Refined point.go and point_test.go with translation fmt.Errof parameters.

--- core/point/point_test.go
+++ core/point/point_test.go
@@ -17,6 +17,8 @@ func TestCreate(t *testing.T) {
 	}
 }
 
+// The below function and related elements were contributed by
+// un nuovo amico GitHub dall'Italia, di nome ilmanzo
 func TestPointToString(t *testing.T) {
 	result := fmt.Sprintf("%v", New(2, 3))
 	const expected = "{2,3}"
@@ -29,50 +31,45 @@ func TestPointToString(t *testing.T) {
 func TestPointToSGFTranslate(t *testing.T) {
 	// First test translation from integer-point to SGF-string-point
 	testToSGFCases := []struct {
-		desc string
-		in   *Point
-		want string
+		desc          string
+		in            *Point
+		want          string
+		wantErrSubstr string
 	}{
 		{
 			desc: "Point => SGF",
-			in: &Point{
-				x: 8,
-				y: 16,
-			},
+			in:   New(8, 16),
 			want: "iq",
 		},
 		{
 			desc: "Point => SGF",
-			in: &Point{
-				x: 12,
-				y: 5,
-			},
+			in:   New(12, 5),
 			want: "mf",
 		},
 		{
 			desc: "Point => SGF",
-			in: &Point{
-				x: 33,
-				y: 8,
-			},
+			in:   New(33, 8),
 			want: "Hi",
 		},
 		{
 			desc: "Point => SGF",
-			in: &Point{
-				x: 40,
-				y: 51,
-			},
+			in:   New(40, 51),
 			want: "OZ",
 		},
+		// // Error Cases
+		// {
+		// 	desc:          "Error Point => SGF",
+		// 	in:            New(40, 52),
+		// 	wantErrSubstr: "Point Y value > 51, out of range",
+		// },
 	}
 
 	for _, tc := range testToSGFCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			toSGFOut, _ := New(tc.in.x, tc.in.y).ToSGF()
-			if toSGFOut != tc.want {
+			sgfOut, _ := New(tc.in.x, tc.in.y).ToSGF()
+			if sgfOut != tc.want {
 				t.Errorf("%q.ToSGF() = %q, but wanted %q", tc.in,
-					toSGFOut, tc.want)
+					sgfOut, tc.want)
 			}
 		})
 	}
@@ -80,59 +77,47 @@ func TestPointToSGFTranslate(t *testing.T) {
 
 func TestSGFToPointTranslate(t *testing.T) {
 	testToPointCases := []struct {
-		desc string
-		in   string
-		want *Point
+		desc          string
+		in            string
+		want          *Point
+		wantErrSubstr string
 	}{
 		{
 			desc: "SGF => Point",
 			in:   "iq",
-			want: &Point{
-				x: 8,
-				y: 16,
-			},
+			want: New(8, 16),
 		},
 		{
 			desc: "SGF => Point",
 			in:   "mf",
-			want: &Point{
-				x: 12,
-				y: 5,
-			},
+			want: New(12, 5),
 		},
 		{
 			desc: "SGF => Point",
 			in:   "Hi",
-			want: &Point{
-				x: 33,
-				y: 8,
-			},
+			want: New(33, 8),
 		},
 		{
 			desc: "SGF => Point",
 			in:   "OZ",
-			want: &Point{
-				x: 40,
-				y: 51,
-			},
+			want: New(40, 51),
 		},
 	}
 
 	for _, tc := range testToPointCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			toPointOut, _ := NewFromSGF(tc.in)
+			pntOut, _ := NewFromSGF(tc.in)
 			// Utilizing the point.go *Point type X Y getters below
-			pointX := toPointOut.X()
-			pointY := toPointOut.Y()
-			if pointX != tc.want.x {
-				t.Errorf("%q.toPointOut.x = %q, but wanted %q", tc.in,
-					pointX, tc.want.x)
+			x := pntOut.X()
+			y := pntOut.Y()
+			if x != tc.want.x {
+				t.Errorf("%q.pntOut.x = %q, but wanted %q", tc.in,
+					x, tc.want.x)
 			}
-			if pointY != tc.want.y {
-				t.Errorf("%q.toPointOut.y = %q, but wanted %q", tc.in,
-					pointY, tc.want.y)
+			if y != tc.want.y {
+				t.Errorf("%q.pntOut.y = %q, but wanted %q", tc.in,
+					y, tc.want.y)
 			}
 		})
 	}
-
 }
