3a7e7a6d5cb16a27c07746b5bdbb35012e50b28c
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-10-10 07:18:19 -0700

    Refined point.go and point_test.go with translation fmt.Errof parameters.

--- core/point/point_test.go
+++ core/point/point_test.go
@@ -2,6 +2,7 @@ package point
 
 import (
 	"fmt"
+	"github.com/otrego/clamshell/core/errcheck"
 	"testing"
 )
 
@@ -17,8 +18,7 @@ func TestCreate(t *testing.T) {
 	}
 }
 
-// The below function and related elements were contributed by
-// un nuovo amico GitHub dall'Italia, di nome ilmanzo
+// The below function TestPointToString() was authored by ilmanzo
 func TestPointToString(t *testing.T) {
 	result := fmt.Sprintf("%v", New(2, 3))
 	const expected = "{2,3}"
@@ -31,10 +31,10 @@ func TestPointToString(t *testing.T) {
 func TestPointToSGFTranslate(t *testing.T) {
 	// First test translation from integer-point to SGF-string-point
 	testToSGFCases := []struct {
-		desc          string
-		in            *Point
-		want          string
-		wantErrSubstr string
+		desc         string
+		in           *Point
+		want         string
+		expErrSubstr string
 	}{
 		{
 			desc: "Point => SGF",
@@ -56,20 +56,49 @@ func TestPointToSGFTranslate(t *testing.T) {
 			in:   New(40, 51),
 			want: "OZ",
 		},
-		// // Error Cases
-		// {
-		// 	desc:          "Error Point => SGF",
-		// 	in:            New(40, 52),
-		// 	wantErrSubstr: "Point Y value > 51, out of range",
-		// },
+		// Error Cases:
+		// Negative X Y values
+		{
+			desc:         "Error: Point => SGF: X value < 0",
+			in:           New(-33, 8),
+			expErrSubstr: "Point X value < 0, out of range",
+		},
+		{
+			desc:         "Error: Point => SGF: Y value < 0",
+			in:           New(33, -8),
+			expErrSubstr: "Point Y value < 0, out of range",
+		},
+		// Out of range X Y values
+		{
+			desc:         "Error: Point => SGF: X value > 51",
+			in:           New(53, 8),
+			expErrSubstr: "Point X value > 51, out of range",
+		},
+		{
+			desc:         "Error: Point => SGF: Y value > 51",
+			in:           New(33, 53),
+			expErrSubstr: "Point Y value > 51, out of range",
+		},
 	}
 
+	// Test cases
 	for _, tc := range testToSGFCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			sgfOut, _ := New(tc.in.x, tc.in.y).ToSGF()
+			sgfOut, err := New(tc.in.x, tc.in.y).ToSGF()
+			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
 			if sgfOut != tc.want {
 				t.Errorf("%q.ToSGF() = %q, but wanted %q", tc.in,
 					sgfOut, tc.want)
+				// if cerr != nil && sgfOut != tc.want {
+				if cerr != nil {
+					t.Fatal(cerr)
+				}
+				if err != nil {
+					return
+				}
+				if sgfOut == "" {
+					t.Fatal("unexpected empty SGF string value")
+				}
 			}
 		})
 	}
@@ -77,10 +106,10 @@ func TestPointToSGFTranslate(t *testing.T) {
 
 func TestSGFToPointTranslate(t *testing.T) {
 	testToPointCases := []struct {
-		desc          string
-		in            string
-		want          *Point
-		wantErrSubstr string
+		desc         string
+		in           string
+		want         *Point
+		expErrSubstr string
 	}{
 		{
 			desc: "SGF => Point",
@@ -102,21 +131,55 @@ func TestSGFToPointTranslate(t *testing.T) {
 			in:   "OZ",
 			want: New(40, 51),
 		},
+		// Error Cases:
+		// Empty string
+		{
+			desc:         "Error: SGF => Point empty string",
+			in:           "",
+			expErrSubstr: "Error: SGF X Y string values empty",
+		},
+		// Short string
+		{
+			desc:         "Error: SGF => Point short string",
+			in:           "Q",
+			expErrSubstr: "SGF X or Y string value missing",
+		},
+		// Empty string
+		{
+			desc:         "Error: SGF => Point long string",
+			in:           "xyZ",
+			expErrSubstr: "SGF X Y string values empty",
+		},
 	}
 
 	for _, tc := range testToPointCases {
 		t.Run(tc.desc, func(t *testing.T) {
-			pntOut, _ := NewFromSGF(tc.in)
+			pntOut, err := NewFromSGF(tc.in)
+			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
 			// Utilizing the point.go *Point type X Y getters below
-			x := pntOut.X()
-			y := pntOut.Y()
-			if x != tc.want.x {
-				t.Errorf("%q.pntOut.x = %q, but wanted %q", tc.in,
-					x, tc.want.x)
-			}
-			if y != tc.want.y {
-				t.Errorf("%q.pntOut.y = %q, but wanted %q", tc.in,
-					y, tc.want.y)
+			if pntOut != tc.want {
+				if pntOut != nil {
+					x := pntOut.X()
+					y := pntOut.Y()
+					if x != tc.want.x {
+						t.Errorf("%q.pntOut.x = %q, but wanted %q",
+							tc.in, x, tc.want.x)
+					}
+					if y != tc.want.y {
+						t.Errorf("%q.pntOut.y = %q, but wanted %q",
+							tc.in,
+							y, tc.want.y)
+					}
+					if cerr != nil {
+						t.Fatal(cerr)
+					}
+					if err != nil {
+						return
+					}
+				}
+				if pntOut == nil {
+					t.Fatal("unexpected nil point integer value")
+				}
 			}
 		})
 	}
