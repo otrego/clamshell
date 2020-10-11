cf2b3373a5104a01ca437abf50bf3e0a68d56eb7
Josh Hoak <jrhoak@gmail.com>
2020-10-11 12:09:57 -0700

    finish query cleanup.

--- core/katago/query.go
+++ core/katago/query.go
@@ -3,6 +3,7 @@ package katago
 import (
 	"encoding/json"
 	"fmt"
+	"math"
 	"strconv"
 
 	"github.com/google/uuid"
@@ -35,10 +36,10 @@ type Query struct {
 	Komi *float64 `json:"komi,omitempty"`
 
 	// BoardXSize is the width of the board
-	BoardXSize *int `json:"boardXSize,omitempty"`
+	BoardXSize int `json:"boardXSize,omitempty"`
 
 	// BoardXSize is the width of the board
-	BoardYSize *int `json:"boardYSize,omitempty"`
+	BoardYSize int `json:"boardYSize,omitempty"`
 
 	// AnalyzeTurns is the turns of the game to analyze. If this field is not
 	// specified, defaults to analyzing the last turn only.
@@ -98,11 +99,18 @@ func (q *Query) ToJSON() ([]byte, error) {
 	return json.Marshal(q)
 }
 
+// gameConverter is used to convert games into query objects.
 type gameConverter struct {
 	g *game.Game
 }
 
+// point converts a point to a GTP point
 func (gc *gameConverter) point(pt *point.Point) string {
+	if pt == nil {
+		// We treat nil points as passes.
+		return "pass"
+	}
+
 	const a = 'A'
 	const i = 'I'
 	val := rune(a + pt.X())
@@ -113,10 +121,14 @@ func (gc *gameConverter) point(pt *point.Point) string {
 	return fmt.Sprintf("%c%d", val, pt.Y()+1)
 }
 
+// move converts from a game-move to a move-array with a GTP Point. This is a
+// format peculiar to Katago.
 func (gc *gameConverter) move(mv *game.Move) Move {
 	return Move{string(mv.Color()), gc.point(mv.Point())}
 }
 
+// initialStones converts the initial placements (e.g., handicap stones) into
+// the initial stones for the analysis.
 func (gc *gameConverter) initialStones() []Move {
 	var out []Move
 	for _, mv := range gc.g.Root.Placements {
@@ -125,6 +137,8 @@ func (gc *gameConverter) initialStones() []Move {
 	return out
 }
 
+// initialPlayer sets the initial player. By default, katago assumes
+// black-to-play, so this isn't necessary.
 func (gc *gameConverter) initialPlayer() string {
 	if val, ok := gc.g.Root.Properties["PL"]; ok && len(val) > 0 {
 		return val[0]
@@ -132,18 +146,16 @@ func (gc *gameConverter) initialPlayer() string {
 	return ""
 }
 
-func (gc *gameConverter) mainBranchMoves(maxMoves int) []Move {
-	var out []Move
+// mainBranchMoves gets moves along the primary branch (0th-variation).
+func (gc *gameConverter) mainBranchMoves() []Move {
+	out := []Move{}
 	idx := 0
 	for n := gc.g.Root; ; n = n.Children[0] {
-		if n.Move != nil && !n.Move.IsPass() {
+		if n.Move != nil {
 			out = append(out, gc.move(n.Move))
 		}
 		if len(n.Children) == 0 {
 			// No more children; terminate traversal.
-			break
-		}
-		if idx >= maxMoves {
 			break
 		}
 		idx++
@@ -151,6 +163,7 @@ func (gc *gameConverter) mainBranchMoves(maxMoves int) []Move {
 	return out
 }
 
+// rules gets the relevant rule-set, returning TrompTaylorRules if not provided.
 func (gc *gameConverter) rules() Rules {
 	if val, ok := gc.g.Root.Properties["RU"]; ok && len(val) > 0 {
 		return Rules(val[0])
@@ -158,70 +171,119 @@ func (gc *gameConverter) rules() Rules {
 	return TrompTaylorRules
 }
 
+// komi gets the komi value, parsed as a float. Note that the decimal-part can
+// only be exactl 0.5 or 0.
 func (gc *gameConverter) komi() (*float64, error) {
 	if val, ok := gc.g.Root.Properties["KM"]; ok && len(val) > 0 {
 		km, err := strconv.ParseFloat(val[0], 64)
 		if err != nil {
 			return nil, err
 		}
+		_, fp := math.Modf(km)
+		if !(fp == 0.5 || fp == 0.0) {
+			return nil, fmt.Errorf("the only decimal-value allowed for komi is 0.0 or 0.5. komi was %f", km)
+		}
 		return &km, nil
 	}
 	return nil, nil
 }
 
-func (gc *gameConverter) boardSize() (*int, error) {
+// boardSize gets the size of the go board. Only sizes ups to 25 are allowed,
+// but should typically be 19, 13, or 9.
+func (gc *gameConverter) boardSize() (int, error) {
 	if val, ok := gc.g.Root.Properties["SZ"]; ok && len(val) > 0 {
 		sz, err := strconv.Atoi(val[0])
 		if err != nil {
-			return nil, err
+			return 0, err
 		}
 		if sz > 25 {
-			return nil, fmt.Errorf("only sizes up to 25 are supported")
+			return 0, fmt.Errorf("only sizes up to 25 are supported")
 		}
-		return &sz, nil
+		return sz, nil
 	}
-	return nil, nil
+	return 19, nil
 }
 
-func (gc *gameConverter) analyzeMainBranch(maxMoves int) []int {
+// analyzeMainBranch analyzes the main branch of the game.
+func (gc *gameConverter) analyzeMainBranch(startFrom, maxMoves int) []int {
 	var out []int
-	idx := 0
-	for n := gc.g.Root; ; n = n.Children[0] {
-		if n.Move != nil && !n.Move.IsPass() {
-			out = append(out, idx)
-		} else if n.Move != nil && n.Move.IsPass() {
-			// stop analyzing at first pass
-			break
+	numAnalyzed := 0
+	if maxMoves == 0 {
+		maxMoves = 1000
+	}
+	for n := gc.g.Root; n != nil; n = n.Next(0) {
+		if n.MoveNum() >= startFrom && numAnalyzed < maxMoves {
+			out = append(out, n.MoveNum())
+			numAnalyzed++
 		}
-		if len(n.Children) == 0 {
-			// No more children; terminate traversal.
-			break
-		}
-		if idx >= maxMoves {
-			break
-		}
-		idx++
 	}
 	return out
 }
 
-func newInt(i int) *int {
+// NewInt creates an integer pointer.
+func NewInt(i int) *int {
 	return &i
 }
 
-// MainBranchSurvey does a full game analysis of the main-branch.
-func MainBranchSurvey(g *game.Game) (*Query, error) {
+// QueryOptions contains options for constructing a Query from a game.
+type QueryOptions struct {
+	// MaxMoves is used to provide a max when analyzing moves. By default, analyze the
+	// whole game. Largely used for debugging.
+	MaxMoves *int
+
+	// StartFrom indicates which move to start from. By default, start from the
+	// first move (but not the root).
+	StartFrom *int
+
+	// AnalysisDepth indicates how deep to print analyses. If not specified,
+	// defaults to 0 (no depth to analysis).
+	AnalysisDepth *int
+
+	// MaxVisits indicates the maximum number of visits to perform during
+	// analysis. If not specified, defaults to 10.
+	MaxVisits *int
+}
+
+// defaultOptions adds defaults to query options
+func defaultOptions(opts *QueryOptions) *QueryOptions {
+	// defaults for the query options.
+	d := &QueryOptions{
+		MaxMoves:      NewInt(0),
+		StartFrom:     NewInt(1),
+		AnalysisDepth: NewInt(0),
+		MaxVisits:     NewInt(10),
+	}
+	if opts == nil {
+		opts = &QueryOptions{}
+	}
+
+	if opts.MaxMoves != nil {
+		d.MaxMoves = opts.MaxMoves
+	}
+	if opts.StartFrom != nil {
+		d.StartFrom = opts.StartFrom
+	}
+	if opts.AnalysisDepth != nil {
+		d.AnalysisDepth = opts.AnalysisDepth
+	}
+	if opts.MaxVisits != nil {
+		d.MaxVisits = opts.MaxVisits
+	}
+
+	return d
+}
+
+// AnalysisQueryFromGame creates a Katago query object from a go-game.
+func AnalysisQueryFromGame(g *game.Game, inOpts *QueryOptions) (*Query, error) {
 	q := NewQuery()
 	gc := &gameConverter{g: g}
-
-	// TODO(kashomon): This is for just testing.
-	maxMoves := 500
+	opts := defaultOptions(inOpts)
 
 	q.InitialStones = gc.initialStones()
 	q.InitialPlayer = gc.initialPlayer()
-	q.Moves = gc.mainBranchMoves(maxMoves)
+	q.Moves = gc.mainBranchMoves()
 	q.Rules = gc.rules()
-	q.MaxVisits = newInt(10)
+	q.MaxVisits = opts.MaxVisits
 
 	km, err := gc.komi()
 	if err != nil {
@@ -233,14 +295,11 @@ func MainBranchSurvey(g *game.Game) (*Query, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	q.BoardYSize = sz
 	q.BoardXSize = sz
-
-	// Only show one move deep
-	q.OverrideSettings["analysisPVLen"] = "1"
-
-	q.AnalyzeTurns = gc.analyzeMainBranch(maxMoves)
-	// q.AnalyzeTurns = gc.analyzeMainBranch(5)
+	q.OverrideSettings["analysisPVLen"] = strconv.Itoa(*opts.AnalysisDepth)
+	q.AnalyzeTurns = gc.analyzeMainBranch(*opts.StartFrom, *opts.MaxMoves)
 
 	return q, nil
 }
