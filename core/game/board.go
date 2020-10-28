5517fac5135fbe73251d8d623712c94e3f53932c
TrevorPeters <TrevorPeters@users.noreply.github.com>
2020-10-28 03:12:57 -0700

    finished implementing AddStone

--- core/game/board.go
+++ core/game/board.go
@@ -1,7 +1,5 @@
 package game
 
-//plan for capture is to bfs every group we find for any liberties.
-//make sure to mark expanded nodes!!
 import (
 	"container/list"
 	"fmt"
@@ -12,63 +10,87 @@ import (
 )
 
 // Board TODO
-type Board [][]color.Color
+type Board struct {
+	board          [][]color.Color
+	capturedStones []*point.Point
+	ko             *point.Point
+}
 
 // NewBoard creates a new size x size board.
 func NewBoard(size int) *Board {
-	var board Board = make([][]color.Color, size)
+	board := Board{
+		make([][]color.Color, size),
+		nil,
+		nil,
+	}
+
 	for i := 0; i < size; i++ {
-		board[i] = make([]color.Color, size)
+		board.board[i] = make([]color.Color, size)
 	}
 	return &board
 }
 
 // AddStone adds a stone to the board. TODO
-func (board Board) AddStone(pt *point.Point, c color.Color) error {
+func (b *Board) AddStone(m *Move) error {
+	pt := m.Point()
 	var x, y int = int(pt.X()), int(pt.Y())
-	switch {
-	case x >= len(board[0]) || y >= len(board) ||
-		x < 0 || y < 0:
-		return fmt.Errorf("move %v out of bound for %dx%d board",
-			pt, len(board[0]), len(board))
-	case board[y][x] != color.Empty:
+	var ko *point.Point = b.ko
+	b.ko = nil
+	b.capturedStones = make([]*point.Point, 0)
+
+	if x >= len(b.board[0]) || y >= len(b.board) ||
+		x < 0 || y < 0 {
+		return fmt.Errorf("move %v out of bounds for %dx%d board",
+			pt, len(b.board[0]), len(b.board))
+	}
+	if b.board[y][x] != color.Empty {
 		return fmt.Errorf("move %v already occupied", pt)
-	case false:
-		return fmt.Errorf("move %v is suicidal", pt)
-	case false:
-		return fmt.Errorf("%v is an illigel ko move", pt)
 	}
 
-	board[y][x] = c
+	b.board[y][x] = m.Color()
+	b.FindEnemyGroups(m)
+	if len(b.capturedStones) == 0 && len(b.CapturedStones(m.Point())) != 0 {
+		b.board[y][x] = color.Empty
+		return fmt.Errorf("move %v is suicidal", pt)
+	}
+	if len(b.capturedStones) == 1 && *ko == *(b.capturedStones[0]) {
+		b.board[y][x] = color.Empty
+		return fmt.Errorf("%v is an illegal ko move", pt)
+	}
+	b.RemoveCapturedStones()
 	return nil
 }
 
-// FindEnemyGroups looks for all the enemy groups next to the stone
-// that was placed at Point pt.
-func (board Board) FindEnemyGroups(pt *point.Point, c color.Color) {
+// FindEnemyGroups returns a list of captured stones TODO
+func (b *Board) FindEnemyGroups(m *Move) []*point.Point {
+	pt := m.Point()
 	points := make([]*point.Point, 4)
 	points[0] = point.New(pt.X()+1, pt.Y())
 	points[1] = point.New(pt.X()-1, pt.Y())
 	points[2] = point.New(pt.X(), pt.Y()+1)
 	points[3] = point.New(pt.X(), pt.Y()-1)
+	capturedStones := make([]*point.Point, 0)
 	for _, point := range points {
-		captured, capPoints := board.IsCaptured(point)
-		if captured {
-			for _, capPoint := range capPoints {
-				board[capPoint.Y()][capPoint.X()] = color.Empty
-			}
-		}
+		capturedStones = append(capturedStones, b.CapturedStones(point)...)
+	}
+	b.capturedStones = capturedStones
+	return capturedStones
+}
+
+// RemoveCapturedStones removes the stones in points from board.
+func (b *Board) RemoveCapturedStones() {
+	for _, point := range b.capturedStones {
+		b.board[point.Y()][point.X()] = color.Empty
 	}
 }
 
-// IsCaptured looks at the group at Point pt and returns true if that
-// group is captured, and false otherwise.
-func (board Board) IsCaptured(pt *point.Point) (bool, []*point.Point) {
+// CapturedStones returns the captured stones in group containing Point pt.
+// returns nil if no stones were captured
+func (b *Board) CapturedStones(pt *point.Point) []*point.Point {
 	expanded := make(map[point.Point]bool)
 	// current group color
-	c := board[pt.Y()][pt.X()]
+	c := b.board[pt.Y()][pt.X()]
 	// becomes true only if a liberty is found for this group
-	captured := true
 	queue := list.New()
 	queue.PushBack(pt)
 	for queue.Len() > 0 {
@@ -79,46 +101,43 @@ func (board Board) IsCaptured(pt *point.Point) (bool, []*point.Point) {
 			panic("e.Value was not of type point.Point")
 		}
 		x, y := int(pt1.X()), int(pt1.Y())
-		if x >= len(board[0]) || y >= len(board) ||
+		if x >= len(b.board[0]) || y >= len(b.board) ||
 			x < 0 || y < 0 {
 			continue
-		} else if board[y][x] == color.Empty {
-			captured = false
+		} else if b.board[y][x] == color.Empty {
+			// Liberty has been found, no need to continue search
 			var stoneGroup []*point.Point = nil
-			return captured, stoneGroup
-		} else if board[y][x] == c && !expanded[*pt1] {
+			return stoneGroup
+		} else if b.board[y][x] == c && !expanded[*pt1] {
 			expanded[*pt1] = true
-			// enqueue right
-			queue.PushBack(point.New(pt1.X()+1, pt1.Y()))
-			// enqueue left
-			queue.PushBack(point.New(pt1.X()-1, pt1.Y()))
-			// enqueue down
-			queue.PushBack(point.New(pt1.X(), pt1.Y()+1))
-			// enqueue up
-			queue.PushBack(point.New(pt1.X(), pt1.Y()-1))
+			queue.PushBack(point.New(pt1.X()+1, pt1.Y())) // enqueue right
+			queue.PushBack(point.New(pt1.X()-1, pt1.Y())) // enqueue left
+			queue.PushBack(point.New(pt1.X(), pt1.Y()+1)) // enqueue down
+			queue.PushBack(point.New(pt1.X(), pt1.Y()-1)) // enqueue up
 		}
 	}
-	stoneGroup := make([]*point.Point, len(expanded)) //the stones that were captured
+	// The stones that were captured
+	stoneGroup := make([]*point.Point, len(expanded))
 	i := 0
 	for key := range expanded {
 		stoneGroup[i] = point.New(key.X(), key.Y())
 		i++
 	}
-	return captured, stoneGroup
+	return stoneGroup
 }
 
 // String returns a string representation of this board.
-func (board Board) String() string {
+func (b *Board) String() string {
 	var sb strings.Builder
-	for i := 0; i < len(board); i++ {
+	for i := 0; i < len(b.board); i++ {
 		// To increase useability of this String function,
 		// color.Empty is converted from "" to ".".
-		str := make([]string, len(board[0]))
-		for j := 0; j < len(board[0]); j++ {
-			if board[i][j] == color.Empty {
+		str := make([]string, len(b.board[0]))
+		for j := 0; j < len(b.board[0]); j++ {
+			if b.board[i][j] == color.Empty {
 				str[j] = "."
 			} else {
-				str[j] = string(board[i][j])
+				str[j] = string(b.board[i][j])
 			}
 		}
 		sb.WriteString(fmt.Sprintf("%v\n", str))
