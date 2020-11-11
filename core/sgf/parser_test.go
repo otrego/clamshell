b6871e7173d72f4a184ea959bb57323a5815878f
TrevorPeters <TrevorPeters@users.noreply.github.com>
2020-11-11 15:04:39 -0800

    added function Serialize that converts a game to sgf

--- core/sgf/parser_test.go
+++ core/sgf/parser_test.go
@@ -323,3 +323,57 @@ func TestPropertyPostProcessing(t *testing.T) {
 		})
 	}
 }
+
+func TestWrite(t *testing.T) {
+	testCases := []struct {
+		desc string
+		sgf  string
+	}{
+		{
+			desc: "complex problem",
+			sgf: `
+(;GM[1]FF[4]CA[UTF-8]AP[Glift]ST[2]
+RU[Japanese]SZ[19]KM[0.00]
+C[Black to play. There aren't many options
+to choose from, but you might be surprised at the answer!]
+PW[White]PB[Black]AW[pa][qa][nb][ob][qb][oc][pc][md][pd][ne][oe]
+AB[na][ra][mb][rb][lc][qc][ld][od][qd][le][pe][qe][mf][nf][of][pg]
+(;B[mc]
+	;W[nc]C[White lives.])
+(;B[ma]
+	(;W[oa]
+		;B[nc]
+		;W[nd]
+		;B[mc]C[White dies.]GB[1])
+	(;W[mc]
+		(;B[oa]
+		;W[nd]
+		;B[pb]C[White lives])
+		(;B[nd]
+			;W[nc]
+			;B[oa]C[White dies.]GB[1]))
+	(;W[nd]
+		;B[mc]
+		;W[oa]
+		;B[nc]C[White dies.]GB[1]))
+(;B[nc]
+	;W[mc]C[White lives])
+(;B[]C[A default consideration]
+	;W[mc]C[White lives easily]))`,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			g, err := sgf.Parse(tc.sgf)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			got, err := sgf.Parse(sgf.Serialize(g))
+			if !cmp.Equal(got, g) {
+				t.Error("Error")
+			}
+		})
+	}
+}
