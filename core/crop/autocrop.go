af1d4b6725e7c0ff18b6fb8402f1724401b12d4a
jonathanmli <detective.redd@gmail.com>
2020-12-19 00:30:43 -0800

    formatted bodoko code to follow clamshell aesthetics

--- core/crop/autocrop.go
+++ core/crop/autocrop.go
@@ -1,1 +1,12 @@
 package crop
+
+// import (
+// 	"github.com/otrego/clamshell/core/movetree"
+// )
+
+// //FromMovetree : not sure what this does tbh (nor if it is complete), ported over from bodoko
+// func FromMovetree(mt *movetree.Movetree) *Cropping {
+// 	ints := mt.getIntersections()
+// 	middle := ints / 2
+// 	return FromPreset(All)
+// }
