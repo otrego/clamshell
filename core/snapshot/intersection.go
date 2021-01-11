26f239c3f88f1268d6e917638c792ecf072ca3d9
Josh Hoak <jrhoak@gmail.com>
2021-01-10 17:36:05 -0800

    More work on snapshot process

---
+++ core/snapshot/intersection.go
@@ -0,0 +1,8 @@
+package snapshot
+
+// Intersection contains a single intersection
+type Intersection struct {
+	BaseLayer Symbol
+}
+
+type Symbol int
--- core/snapshot/snapshot.go
+++ core/snapshot/snapshot.go
@@ -1,38 +1,38 @@
-// Package snapshot makes a flattened, abstracted snapshot of a particular game
+// Package snapshot makes a flattened, abstracted snapshot of a game
 // position.
 //
 // This is based on the flattener logic from glift-core:
 // https://github.com/Kashomon/glift-core/tree/master/src/flattener
 package snapshot
 
-import "github.com/otrego/clamshell/core/movetree"
+import (
+	"github.com/otrego/clamshell/core/bbox"
+	"github.com/otrego/clamshell/core/movetree"
+)
 
-// Options contains options for creating snapshots.
+// Options contains options for creating flattened snapshots.
 type Options struct {
 }
 
-// New creates a new Snapshot from a given movetree and path.
-func New(mt *movetree.MoveTree, position movetree.Path, opts *Options) *Snapshot {
-	return &Snapshot{
-		OriginalSize: mt.Root.GameInfo.Size,
-	}
+// Create a new Snapshot from a given movetree and path.
+func Create(mt *movetree.MoveTree, position movetree.Path, opts *Options) *Snapshot {
+	return &Snapshot{}
 }
 
-// A snapshot represents a specific board position, which can be used during
+// A Snapshot represents a specific board position, which can be used during
 // image generation.
-//
-// **Snapshots are intended to be immutable.
 type Snapshot struct {
-	// OriginalSize of the Go-Board. The actual size is likely less due to
-	// cropping.
-	OriginalSize int
+
+	// Comment for the position.
+	Comment string
+
+	// Borad contains the symbolic representation of the display of the board.
+	Board *Board
 }
 
-// Board is a snapshot-board representation. It is different than the game-logic
-// board in that it is frequently cropped and designed for rendering.
+// Board is a snapshot-board representation. It can be cropped.
 type Board struct {
 	Intersections [][]*Intersection
-}
 
-type Intersection struct {
+	CropBox *bbox.CropBox
 }
