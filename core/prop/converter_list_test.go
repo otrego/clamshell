2df16716c62157df3502557f44bf775ad66e1c21
Josh Hoak <jrhoak@gmail.com>
2021-01-20 22:58:28 -0800

    Make some commen helpers for property-conversion. (#206)
    This PR makes some common helpers for property conversion and uses this
    for moves_test and the converter_list_test.

--- core/prop/converter_list_test.go
+++ core/prop/converter_list_test.go
@@ -15,15 +15,65 @@ type propGetter func(*movetree.Node) interface{}
 
 type setprops func(*movetree.Node) *movetree.Node
 
-func TestConverters_From(t *testing.T) {
-	testCases := []struct {
-		desc string
-		prop string
-		data []string
+type fromSGFTestCase struct {
+	desc         string
+	prop         string
+	data         []string
+	makeExpNode  func(*movetree.Node)
+	expErrSubstr string
+}
 
-		makeExpNode  func(n *movetree.Node)
-		expErrSubstr string
-	}{
+func testConvertFromSGFCases(t *testing.T, testCases []fromSGFTestCase) {
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			n := movetree.NewNode()
+			expNode := movetree.NewNode()
+			tc.makeExpNode(expNode)
+			err := ProcessPropertyData(n, tc.prop, tc.data)
+			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
+			if cerr != nil {
+				t.Fatal(cerr)
+			}
+			if err != nil {
+				return
+			}
+			if !reflect.DeepEqual(n, expNode) {
+				t.Errorf("got node %v, but expected node %v", n, expNode)
+			}
+		})
+	}
+}
+
+type convertNodeTestCase struct {
+	desc         string
+	makeNode     func(*movetree.Node)
+	expOut       string
+	expErrSubstr string
+}
+
+func testConvertNodeCases(t *testing.T, testCases []convertNodeTestCase) {
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			node := movetree.NewNode()
+			tc.makeNode(node)
+			out, err := ConvertNode(node)
+			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
+			if cerr != nil {
+				t.Fatal(cerr)
+			}
+			if err != nil {
+				return
+			}
+
+			if out != tc.expOut {
+				t.Errorf("ConvertNode(%v)=%v, but expected %v", node, out, tc.expOut)
+			}
+		})
+	}
+}
+
+func TestConvertFromSGF_Collection(t *testing.T) {
+	testCases := []fromSGFTestCase{
 		{
 			desc: "black placements",
 			prop: "AB",
@@ -130,34 +180,11 @@ func TestConverters_From(t *testing.T) {
 		},
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			n := movetree.NewNode()
-			expNode := movetree.NewNode()
-			tc.makeExpNode(expNode)
-			err := ProcessPropertyData(n, tc.prop, tc.data)
-			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
-			if cerr != nil {
-				t.Fatal(cerr)
-			}
-			if err != nil {
-				return
-			}
-			if !reflect.DeepEqual(n, expNode) {
-				t.Errorf("got node %v, but expected node %v", n, expNode)
-			}
-		})
-	}
+	testConvertFromSGFCases(t, testCases)
 }
 
-func TestConverters_ConvertNode(t *testing.T) {
-	testCases := []struct {
-		desc     string
-		makeNode func(*movetree.Node)
-
-		expOut       string
-		expErrSubstr string
-	}{
+func TestConvertNode_Collection(t *testing.T) {
+	testCases := []convertNodeTestCase{
 		{
 			desc: "black placements",
 			makeNode: func(n *movetree.Node) {
@@ -298,22 +325,5 @@ func TestConverters_ConvertNode(t *testing.T) {
 		},
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			node := movetree.NewNode()
-			tc.makeNode(node)
-			out, err := ConvertNode(node)
-			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
-			if cerr != nil {
-				t.Fatal(cerr)
-			}
-			if err != nil {
-				return
-			}
-
-			if out != tc.expOut {
-				t.Errorf("ConvertNode(%v)=%v, but expected %v", node, out, tc.expOut)
-			}
-		})
-	}
+	testConvertNodeCases(t, testCases)
 }
