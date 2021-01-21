da2909a2ca99e976e62a349b9c51fe43fc2ef8b2
Josh Hoak <jrhoak@gmail.com>
2021-01-20 21:55:44 -0800

    Make some commen helpers for property-conversion.
    This PR makes some common helpers for property conversion and uses this
    for moves_test and the converter_list_test.

--- core/prop/moves_test.go
+++ core/prop/moves_test.go
@@ -1,25 +1,16 @@
 package prop
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/otrego/clamshell/core/color"
-	"github.com/otrego/clamshell/core/errcheck"
 	"github.com/otrego/clamshell/core/move"
 	"github.com/otrego/clamshell/core/movetree"
 	"github.com/otrego/clamshell/core/point"
 )
 
-func TestMovesConverter_FromSGF(t *testing.T) {
-	testCases := []struct {
-		desc string
-		prop string
-		data []string
-
-		makeExpNode  func(n *movetree.Node)
-		expErrSubstr string
-	}{
+func TestConvertFromSGF_Moves(t *testing.T) {
+	testCases := []fromSGFTestCase{
 		{
 			desc: "black move: pass",
 			prop: "B",
@@ -54,34 +45,11 @@ func TestMovesConverter_FromSGF(t *testing.T) {
 		},
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			n := movetree.NewNode()
-			expNode := movetree.NewNode()
-			tc.makeExpNode(expNode)
-			err := ProcessPropertyData(n, tc.prop, tc.data)
-			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
-			if cerr != nil {
-				t.Fatal(cerr)
-			}
-			if err != nil {
-				return
-			}
-			if !reflect.DeepEqual(n, expNode) {
-				t.Errorf("got node %v, but expected node %v", n, expNode)
-			}
-		})
-	}
+	testConvertFromSGFCases(t, testCases)
 }
 
-func TestMovesConverter_ConvertNode(t *testing.T) {
-	testCases := []struct {
-		desc     string
-		makeNode func(*movetree.Node)
-
-		expOut       string
-		expErrSubstr string
-	}{
+func TestConvertNode_Moves(t *testing.T) {
+	testCases := []convertNodeTestCase{
 		{
 			desc: "black move: pass",
 			makeNode: func(n *movetree.Node) {
@@ -105,22 +73,5 @@ func TestMovesConverter_ConvertNode(t *testing.T) {
 		},
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			node := movetree.NewNode()
-			tc.makeNode(node)
-			out, err := ConvertNode(node)
-			cerr := errcheck.CheckCases(err, tc.expErrSubstr)
-			if cerr != nil {
-				t.Fatal(cerr)
-			}
-			if err != nil {
-				return
-			}
-
-			if out != tc.expOut {
-				t.Errorf("ConvertNode(%v)=%v, but expected %v", node, out, tc.expOut)
-			}
-		})
-	}
+	testConvertNodeCases(t, testCases)
 }
