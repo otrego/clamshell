1a7c0970bdfca7c6308723ebf924293be38115a1
Mike Hoak <mdhoak@gmail.com>
2021-03-15 20:33:01 -0700

    Adds Terraform variables for prod.

--- tf/prod/main.tf
+++ tf/prod/main.tf
@@ -9,7 +9,7 @@ module "api_server_mod" {
     source = "../api_server_mod"
     dns_managed_zone = "otrego-prod"
     dns_name = "www.otrego.com."
-    project_id = "otrego-dev"
+    project_id = "otrego-prod"
     subnetwork_project = "otrego-prod"
     api_docker_image = var.api_docker_image
     cos_image_name = "cos-stable-85-13310-1209-17"
