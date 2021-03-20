a7aabef70b0f2d2e7ba3a83bc5caaf8a12521717
Josh Hoak <jrhoak@gmail.com>
2021-03-20 11:35:00 -0700

    Create a basic website with assets. (#245)
    * Create a basic website with assets.
    
    This removes GRPC for the time being since it's much easier to do with
    vanilla HTTP. I'd like to add gRPC if possibel.
    
    Fixes:238
    
    * fix linting, rm shadow
    
    * fix lint
    
    * rm shadow stage

---
+++ server/assets/assets.go
@@ -0,0 +1,17 @@
+// Package assets contains static assets for the website
+package assets
+
+import "embed"
+
+// Assets embeds the assets into the go binary.
+//go:embed index.html
+var Assets embed.FS
+
+// Index is the embedded index page.
+var Index = func() []byte {
+	data, err := Assets.ReadFile("index.html")
+	if err != nil {
+		panic(err)
+	}
+	return data
+}()
